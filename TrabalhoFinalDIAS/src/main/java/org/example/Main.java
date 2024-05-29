2f5d7fb86a041b31a0036786681d2103ea624e84
Marcelo Ferreira <1231546@isep.ipp.pt>
2024-05-29 18:41:42 +0100

    Ligação à base de dados criada

--- TrabalhoFinalDIAS/src/main/java/org/example/Main.java
+++ TrabalhoFinalDIAS/src/main/java/org/example/Main.java
@@ -1,7 +1,18 @@
 package org.example;
 
+import org.example.DAO.Ligacao;
+
+import java.sql.Connection;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        Connection con = Ligacao.connection();
+        if (con != null) {
+            System.out.println("Sucesso");
+        }else{
+            System.out.println("Erro");
+        }
+
     }
 }
